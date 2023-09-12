633b6f2ee6c3dbfa40fe00846fa3d710067015a5
Samseen <samson.akanbi@outlook.com>
2023-09-12 17:13:05 -0400

    Project Initialization.

---
+++ src/main/java/com/samseen/resilientnotification/ResilientNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.samseen.resilientnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResilientNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResilientNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samseen/resilientnotification/ResilientNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samseen.resilientnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResilientNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
